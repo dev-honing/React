c03b7db1510a436eb03c8481779ad26c93c8a7e6
Ho Nyeong <dev.honing@gmail.com>
2024-02-24 09:54:19 +0900

    Feat: 배열로 title 적용하기

--- test/src/App.js
+++ test/src/App.js
@@ -1,6 +1,14 @@
+// src/App.js
+import { useState } from "react";
+
 const App = () => {
-  return <div className="App">
-    <h1>리액트 테스트</h1>
-  </div>
-}
+  let [title, setTitle] = useState(["1. 글 제목은 title", "2. title은 h1에 쓴다.", "3. 배열은 멋진 자료형이다."]);
+  return (
+    <div className="App">
+      <h1>{ title[0] }</h1>
+      <h1>{ title[1] }</h1>
+      <h1>{ title[2] }</h1>
+    </div>
+  );
+};
 export default App;
