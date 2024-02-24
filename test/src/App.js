4e7791d3b7e4e8823a4378feac53c856a7b4d6b6
Ho Nyeong <dev.honing@gmail.com>
2024-02-24 10:43:39 +0900

    Feat: 좋아요 기능 추가

--- test/src/App.js
+++ test/src/App.js
@@ -2,12 +2,26 @@
 import { useState } from "react";
 
 const App = () => {
-  let [title, setTitle] = useState(["1. 글 제목은 title", "2. title은 h1에 쓴다.", "3. 배열은 멋진 자료형이다."]);
+  let [title, setTitle] = useState([
+    "1. 글 제목은 title",
+    "2. title은 h1에 쓴다.",
+    "3. 배열은 멋진 자료형이다.",
+  ]);
+  let [like, setLike] = useState(0);
   return (
     <div className="App">
-      <h1>{ title[0] }</h1>
-      <h1>{ title[1] }</h1>
-      <h1>{ title[2] }</h1>
+      <h1>{title[0]}</h1>
+      <div className="like">
+        <button
+          style={{ fontSize: "30px", backgroundColor: "white"}}
+          onClick={() => setLike(like + 1)}
+        >
+          좋아요 {like}
+        </button>
+      </div>
+      <h1>{title[1]}</h1>
+      <h1>{title[2]}</h1>
+      <h1>{title[3]}</h1>
     </div>
   );
 };
